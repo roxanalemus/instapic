477846d64267b30d04462679bc6c8db10054d21d
roxanalemus <roxanalemusdev@gmail.com>
2021-11-07 16:44:51 -0500

    first commit

---
+++ public/main.js
@@ -0,0 +1,72 @@
+var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var trash = document.getElementsByClassName("fa-trash");
+
+Array.from(thumbUp).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const id = this.parentNode.childNodes[1].value
+        // const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const thumbUp =  Number(this.parentNode.children[3].innerText)
+        console.log(id, thumbUp)
+        fetch('thumbUp', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            // 'name': name,
+            // 'msg': msg,
+            'thumbUp':thumbUp,
+            'id': id
+
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+// Array.from(thumbUp).forEach(function(element) {
+//   element.addEventListener('click', function(){
+//     const name = this.parentNode.parentNode.childNodes[1].innerText
+//     const msg = this.parentNode.parentNode.childNodes[3].innerText
+//     const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//     fetch('messages', {
+//       method: 'put',
+//       headers: {'Content-Type': 'application/json'},
+//       body: JSON.stringify({
+//         'name': name,
+//         'msg': msg,
+//         'thumbUp':thumbUp
+//       })
+//     })
+//     .then(response => {
+//       if (response.ok) return response.json()
+//     })
+//     .then(data => {
+//       console.log(data)
+//       window.location.reload(true)
+//     })
+//   });
+// });
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
